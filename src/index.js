e6a188df5bdeb07a89bc49e13f66d48dcbcc324c
Olga Redozubova <olgaredozubova@MacBook-Pro-Olga.local>
2019-04-01 19:55:17 +0300

    recompile

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mathpix_loader_1 = require("./components/mathpix-loader");
-exports.MathpixLoader = mathpix_loader_1.default;
-var mathpix_text_1 = require("./components/mathpix-text");
-exports.MathpixText = mathpix_text_1.default;
-//# sourceMappingURL=index.js.map
